0b8847a0bb960aac726c6264c0dc0f330b66ad13
AshiqAR <ashiqkallara.cet@gmail.com>
2024-09-01 00:18:42 +0530

    view data update requests in hr dashboard

--- admin_dashboard/urls.py
+++ admin_dashboard/urls.py
@@ -16,4 +16,8 @@ urlpatterns = [
     path('leave/review_leave_request/<uuid:leave_request_id>/', views.review_leave_request, name='review_leave_request'),
     path('employee_details/<int:employee_id>/', views.employee_details, name='employee_details'),
     path('employee_update_requests/', views.employee_update_requests, name='employee_update_requests'),
+
+    path('approve-update-request/<int:request_id>/', views.approve_update_request, name='approve_update_request'),
+    path('reject-update-request/<int:request_id>/', views.reject_update_request, name='reject_update_request'),
+    path('edit-and-approve-update-request/', views.edit_and_approve_update_request, name='edit_and_approve_update_request'),
 ]
